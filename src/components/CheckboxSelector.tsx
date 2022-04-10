956277bddf1f33a08c361c14c3142b7298d718ae
akellaX <dodonova.anna76@gmail.com>
2022-04-10 14:44:27 +0300

    CheckboxSelector logic

--- src/components/CheckboxSelector.tsx
+++ src/components/CheckboxSelector.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import { Checkbox, FormControlLabel, FormGroup, FormLabel } from "@mui/material";
 
 type CheckboxSelectorType = {
@@ -8,9 +8,21 @@ type CheckboxSelectorType = {
     onChange: (s: string[]) => void,
 }
 
-export const CheckboxSelector= ({items, label, selected, onChange}: CheckboxSelectorType) => {
-    const list = items.map(item => (<FormControlLabel key={item} control={<Checkbox />} label={item} />))
-
+export const CheckboxSelector = ({ items, label, selected, onChange }: CheckboxSelectorType) => {
+    const onCheckboxChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.checked) {
+            onChange([...selected, e.target.value]);
+        } else {
+            onChange(selected.filter(el => (el !== e.target.value)))
+        }
+    }
+    const list = items.map(item => (
+            <FormControlLabel
+                key={item}
+                control={<Checkbox value={item} onChange={onCheckboxChange}/>}
+                label={item}/>
+        )
+    )
     return (
         <div>
             <FormLabel>{label}</FormLabel>
